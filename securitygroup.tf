73b8a6c5dba70a4e19a2e7436a80c6d40eaf0756
Yan <99516883+yyc3472@users.noreply.github.com>
2022-02-23 23:28:24 -0500

    removed cider blocks

--- securitygroup.tf
+++ securitygroup.tf
@@ -10,8 +10,8 @@ resource "aws_security_group" "allow_web" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["()"]
+    ipv6_cidr_blocks = ["()"]
   }
 
   ingress {
@@ -19,24 +19,24 @@ resource "aws_security_group" "allow_web" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["()"]
+    ipv6_cidr_blocks = ["()"]
   }
   ingress {
     description      = "SSH"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["()"]
+    ipv6_cidr_blocks = ["()"]
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["()"]
+    ipv6_cidr_blocks = ["()"]
   }
 
   tags = {
