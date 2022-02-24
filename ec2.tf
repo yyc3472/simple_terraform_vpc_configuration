3a3bad107016909188165593f743ef5feb424a08
Yan <99516883+yyc3472@users.noreply.github.com>
2022-02-23 23:02:09 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,17 +10,6 @@ resource "aws_instance" "terraform_webserver" {
     network_interface_id = aws_network_interface.terraform_servernic.id
   }
 
-  user_data = <<-E0f
-                #!/bin/bash
-                sudo apt update -y
-                sudo apt install apache2 -y
-                sudo systemctl start apache2
-                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
-                E0f
-
-  tags = {
-    Name = "web-server"
-  }
 
 
 }
