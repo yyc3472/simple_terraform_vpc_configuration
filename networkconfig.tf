5202612b1126f2f628fd8cbb7fb6139e60df850d
Yan <yyc3472@gmail.com>
2022-02-24 19:46:55 -0500

    Update networkconfig.tf

--- networkconfig.tf
+++ networkconfig.tf
@@ -1,37 +1,32 @@
 # 2.internet Gateway
 
-resource "aws_internet_gateway" "gateway" {
-  vpc_id = aws_vpc.terraform_vpc.id
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.prod_vpc.id
 
 
 }
 # 3.route table
 
 resource "aws_route_table" "route_table" {
-  vpc_id = aws_vpc.terraform_vpc.id
+  vpc_id = aws_vpc.prod_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gateway.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.gateway.id
-  }
 
   tags = {
     Name = "prod_routetable"
   }
 }
 
-# 4. Create a subnet
+# 4. subnet_1
 
 
 resource "aws_subnet" "subnet_1" {
-  vpc_id            = aws_vpc.terraform_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id     = aws_vpc.prod_vpc.id
+  cidr_block = "10.0.1.0/25"
 
   tags = {
     Name = "subnet_1"
@@ -40,28 +35,28 @@ resource "aws_subnet" "subnet_1" {
 
 # 5. Associate Subnet with route Table
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "subnet_1_route_table" {
   subnet_id      = aws_subnet.subnet_1.id
   route_table_id = aws_route_table.route_table.id
 }
 
 
 
-#7 Create a network interface with an ip in the subnet that was created in step 4
+#7 network interface with an ip in the subnet
 
-resource "aws_network_interface" "servernic" {
+resource "aws_network_interface" "webserver_nic" {
   subnet_id       = aws_subnet.subnet_1.id
-  private_ips     = ["()"]
-  security_groups = [aws_security_group.allow_web.id]
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_ssh.id]
 
 }
 
-#8 Assign elastic IP to the network interface created in step 7
+#8 Assign elastic IP to the network interface
 
 resource "aws_eip" "elastic_ip" {
   vpc                       = true
-  network_interface         = aws_network_interface.servernic.id
-  associate_with_private_ip = "()"
+  network_interface         = aws_network_interface.webserver_nic.id
+  associate_with_private_ip = "10.0.1.50"
 
-  depends_on = [aws_internet_gateway.gateway]
+  depends_on = [aws_internet_gateway.internet_gateway]
 }
