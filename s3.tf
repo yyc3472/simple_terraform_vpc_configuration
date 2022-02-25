7fad342d209de0cd2a725b6cf0cade579bf191fc
Yan <yyc3472@gmail.com>
2022-02-24 19:38:45 -0500

    Delete s3.tf

--- s3.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket                      = "yanawsbucket"
-    key                         = "terraform/backend/s3/tf_state"
-    skip_credentials_validation = true
-
-  }
-}
