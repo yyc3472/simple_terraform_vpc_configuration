b72e989d9bc3b4d331546caa0cafba06e15ccd92
Yan <yyc3472@gmail.com>
2022-02-24 19:48:14 -0500

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,45 +1,29 @@
 # 6 Security group to allow port 22,80,443
 
-resource "aws_security_group" "allow_web" {
+resource "aws_security_group" "allow_ssh" {
   name        = "allow_web_traffic"
   description = "Allow web traffic"
   vpc_id      = aws_vpc.terraform_vpc.id
 
-  ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["()"]
-    ipv6_cidr_blocks = ["()"]
-  }
 
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["()"]
-    ipv6_cidr_blocks = ["()"]
-  }
   ingress {
     description      = "SSH"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["()"]
-    ipv6_cidr_blocks = ["()"]
+    cidr_blocks      = [0.0.0.0/0]
+   
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["()"]
-    ipv6_cidr_blocks = ["()"]
+    cidr_blocks      = [0.0.0.0/0]
+   
   }
 
   tags = {
-    Name = "allow_WEB"
+    Name = "allow_ssh"
   }
 }
